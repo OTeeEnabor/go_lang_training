b4d2fd464a7ba036cd129c378e328f9b37f42898
unknown <tobienabor@gmail.com>
2023-08-29 18:57:23 +0200

    day 2 learning

---
+++ README.md
@@ -0,0 +1,34 @@
+# Go Lang Training for Demysifying Blockchain Bootcamp
+
+Go has one entry point into the application - main. 
+Once in the entry point, go will run all other functions/applications it sees. 
+
+two methods tp specify entry point
+
+1* package - main
+A folder in Go is considered as a package. 
+2* func main
+A function named main. 
+
+To execute a file in Go - use go run filename.go
+
+### variable declaration
+A variable is anything that has the ability to contain information. It is a placeholder in memory used to reference information. 
+
+long form declaration *(var var_name variable type = value)*
+
+Short form declaration  *(variable := value)* only works within a function. 
+
+### Go Mod Init
+go mod init github repository
+go mod 
+
+## Day 2
+Topics covered
+const
+functions 
+package
+if condition
+for loops
+
+What does go mod tidy do?
--- learn.go
+++ learn.go
@@ -1,15 +1,61 @@
-// entry point into the 
+// entry point into the
 package main
 
 // import necessary packages
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main () {
-	var age int8 = 64 // long form declaration
+	// DAY 2
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your full name: ")
+	name,_ := reader.ReadString('\n')
+	cleanName := strings.Replace(name, "\n","",-1)
+	
+	fmt.Println("Enter current year: ")
+	year,_ := reader.ReadString('\n')
+	cleanYear := strings.Replace(year, "\n","",-1)
+
+	fmt.Println("Enter your date of birth: ")
+	dob,_ := reader.ReadString('\n')
+	cleanDOB := strings.Replace(dob, "\n","",-1)
+
+	// remove spaces
+	cleanName = strings.TrimSpace(cleanName)
+	cleanYear = strings.TrimSpace(cleanYear)
+	cleanDOB = strings.TrimSpace(cleanDOB)
+	
+	// convert to numbers
+	convYear,_ := strconv.ParseInt(cleanYear, 10, 64)
+	convDOB,_ := strconv.ParseInt(cleanDOB, 10, 64)
+
+
+
+	fmt.Printf("name: %v \n", cleanName)
+	fmt.Printf("year :%v \n", convYear)
+	fmt.Printf("dob: %v \n", convDOB)
+	// const studentNumbebr int = 211155217
+	// var firstName string = "Oselu"
+	// var lastName string = "Enabor"
+	// var age int8 = 51
+	// var purchase float32 = 48.40
+	// var isFinalYear bool = false
+	// var favLetter byte ='a'
+	// var countryCurr rune = '$'
+	// var name string = fmt.Sprintf("%v %v", firstName, lastName)
+
+	/*
+	var age int8 = 64 // long form de:claration
 	year := 1959 // short-form declaration, only worksk in function
 	var breadPrice float32 = 19.99
 
 	fmt.Println(age, year,breadPrice)
+	*/
 	/*
 	DISPLAYING INFORMATION
 	fmt.Println("My name is Oselu.")
