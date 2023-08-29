8b223ce88eac588a1540948bbc6cc42bdd6133f3
unknown <tobienabor@gmail.com>
2023-08-29 12:51:15 +0200

    day 1 of Go training with UJ facilitators

---
+++ learn.go
@@ -0,0 +1,19 @@
+// entry point into the 
+package main
+
+// import necessary packages
+import "fmt"
+
+func main () {
+	var age int8 = 64 // long form declaration
+	year := 1959 // short-form declaration, only worksk in function
+	var breadPrice float32 = 19.99
+
+	fmt.Println(age, year,breadPrice)
+	/*
+	DISPLAYING INFORMATION
+	fmt.Println("My name is Oselu.")
+	fmt.Println("My school is UJ")
+	fmt.Print("My campus is APK")
+	*/
+}
