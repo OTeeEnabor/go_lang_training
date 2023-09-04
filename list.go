abd3c2ab63e0e800603d31ffdb09c2473c18abb5
unknown <tobienabor@gmail.com>
2023-09-04 09:14:47 +0200

    arrays and slices in golang

---
+++ list.go
@@ -0,0 +1,18 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var randomFloatNumbers []float64 = []float64{12.34, 23.45, 45.78, 90.8, 0.23}
+// 	shortFloatList := randomFloatNumbers[0:2]
+// 	secondFloatList := shortFloatList[:6]
+
+// 	fmt.Print(secondFloatList)
+
+// 	// var windowShopping []string = []string{"drun stric","rice", "apples"}
+// 	newList := new([]string)
+// }
+// func addThreeNumber(numberOne int64, numberTwo ...int64 ) {
+// 	fmt.Println(numberOne)
+// 	// fmt.Println(numberTwo)varadic functins
+// }
